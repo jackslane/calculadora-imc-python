2c6eede2d98eed738f75ef7e21602babad91b9dd
Jackslane Guerra <jsgh@cesar.school>
2026-09-09 16:14:08 -0300

    Refactor calculator with functions

--- main.py
+++ main.py
@@ -2,10 +2,44 @@ print("=" * 40)
 print ("BEM-VINDO À CALCULADORA DE IMC")
 print("=" * 40)
 
-peso = float(input("Digite seu peso em kg: "))
-altura = float(input("Digite sua altura em metros: "))
+def ler_peso():
+    while True:
+        try:
+            peso = float(input("Digite seu peso em kg: "))
+            if peso <= 0:
+                print("O peso deve ser maior que zero.")
+                continue
+            break
+        except ValueError:
+            print("Digite um número válido.")
 
-imc = peso / (altura ** 2)
+    return peso
+
+peso = ler_peso()
+
+
+
+
+def ler_altura():
+    while True:
+        try:
+            altura = float(input("Digite sua altura em metros: "))
+            if altura <= 0:
+                print("A altura deve ser maior que zero.")
+                continue
+            break
+        except ValueError:
+            print("Digite um número válido.")
+    return altura
+
+altura = ler_altura()
+
+def calcular_imc():
+    imc = peso / (altura ** 2)
+    return imc
+
+imc = calcular_imc()
+
 
 print(f"\nSeu IMC é: {imc:.2f}")
 
