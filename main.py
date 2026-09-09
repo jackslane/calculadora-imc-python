fad75beb298952fad87a41fdbbaac252fbd880c5
Jackslane Guerra <jsgh@cesar.school>
2026-09-09 17:21:12 -0300

    Organize code with comments

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# Cabeçalho
+
 print("=" * 40)
 print ("BEM-VINDO À CALCULADORA DE IMC")
 print("=" * 40)
+
+# Entrada de dados
 
 def ler_peso():
     while True:
@@ -32,11 +36,13 @@ def ler_altura():
             print("Digite um número válido.")
     return altura
 
+# Cálculo
 
 def calcular_imc(peso, altura):
     imc = peso / (altura ** 2)
     return imc
 
+# Classificação
 
 def classificar_imc(imc):
     if imc < 18.5:
@@ -47,6 +53,8 @@ def classificar_imc(imc):
         return "Sobrepeso"
     else:
         return "Obesidade"
+
+# Execução do programa
 
 while True:
     peso = ler_peso()
